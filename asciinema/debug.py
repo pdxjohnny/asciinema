d9a04ff61be774e6f5ecf283a13bed49fc4baa23
John Andersen <john.s.andersen@intel.com>
2016-08-23 08:31:05 -0700

    Need to find out why websockets takes so long to close connection
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- asciinema/debug.py
+++ asciinema/debug.py
@@ -6,9 +6,12 @@ DEBUG_HOST = os.getenv('ASCIINEMA_DEBUG_HOST', 'localhost')
 DEBUG_PORT = int(os.getenv('ASCIINEMA_DEBUG_PORT', 4444))
 
 def write(d):
-    if isinstance(d, str):
-        d = d.encode('UTF-8')
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((DEBUG_HOST, DEBUG_PORT))
-        s.sendall(d)
+    try:
+        if isinstance(d, str):
+            d = d.encode('UTF-8')
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((DEBUG_HOST, DEBUG_PORT))
+            s.sendall(d)
+    except Exception:
+        pass
     return len(d)
--- asciinema/stream.py
+++ asciinema/stream.py
@@ -8,7 +8,8 @@ import asciinema.debug as debug # DEBUG
 
 NEED_INSTALL = True
 try:
-    from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory
+    import asyncio
+    import websockets
     NEED_INSTALL = False
 except ImportError:
     pass
@@ -19,10 +20,10 @@ class Stream(Stdout):
     def __init__(self, max_wait=None):
         super().__init__(max_wait)
         if NEED_INSTALL:
-            print('You need to install websocket-client to stream')
-            print('pip3 install websocket-client')
+            print('You need to install asyncio and websockets to stream')
+            print('pip3 install asyncio websockets')
             sys.exit(1)
-        self.ws_url = 'ws://localhost:5555'
+        self.ws_url = 'ws://localhost:5555/ws'
         parent_conn, child_conn = Pipe()
         self.websocket_child = Process(target=start_websocket, args=(child_conn, self.ws_url,))
         self.websocket_child.start()
@@ -32,13 +33,17 @@ class Stream(Stdout):
         text = self.decoder.decode(data)
         if text:
             delay = round(self._increment_elapsed_time(), 6)
-            self.send_frame([delay, text])
+            self.send_frame({
+                'frame': {
+                    'delay': delay,
+                    'text': text
+                    }
+                })
 
         return len(data)
 
     def close(self):
         debug.write(b'\n\nCLOSING\n') # DEBUG
-        self._increment_elapsed_time()
         self.websocket.send('SHUTDOWN')
         self.websocket.close()
         self.websocket_child.join()
@@ -48,27 +53,33 @@ class Stream(Stdout):
 
 def start_websocket(child_conn, ws_url):
     async def hello():
-        running = True
-        async with websockets.connect(ws_url) as websocket:
-            while running:
+        async with websockets.connect(ws_url, origin='localhost') as websocket:
+            while True:
                 ready = select.select([child_conn], [], [child_conn], 5.0)
                 if len(ready[0]) > 0:
                     try:
                         msg = ready[0][0].recv()
                         if msg == 'SHUTDOWN':
-                            debug.write(b'GOT SHUTDOWN') # DEBUG
-                            running = False
-                        # debug.write(msg) # DEBUG
-                        # debug.write(b'\n') # DEBUG
-                        await websocket.send(msg)
-                        await websocket.recv()
+                            debug.write(b'GOT SHUTDOWN\n') # DEBUG
+                            break
+                        else:
+                            # debug.write(msg) # DEBUG
+                            # debug.write(b'\n') # DEBUG
+                            await websocket.send(msg)
+                            await websocket.recv()
                     except Exception as e:
                         debug.write(str(e)) # DEBUG
-                        running = False
+                        break
                 elif len(ready[2]) > 0:
                     debug.write(b'SERVER CLOSED WS') # DEBUG
-                    running = False
+                    break
+            debug.write(b'CLOSING CHILD CONN\n') # DEBUG
             child_conn.close()
+            debug.write(b'CHILD CONN CLOSED\n') # DEBUG
+            debug.write(b'CLOSING WEBSOCKET\n') # DEBUG
+            websocket.close()
+            debug.write(b'WEBSOCKET CLOSED\n') # DEBUG
+        debug.write(b'\nEND OF HELLO\n') # DEBUG
 
     asyncio.get_event_loop().run_until_complete(hello())
     debug.write(b'\n\nEVENT LOOP COMPLETE\n') # DEBUG
